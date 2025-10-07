1128ae7c4f4e4ada8cbcf6ab6d12d73144119e43
Pololac <lacostepaul@yahoo.fr>
2025-10-08 00:37:15 +0200

    Bootstrap Java App: dependencies, application-*.yml, maven wrapper,

---
+++ backend/src/test/java/com/powerme/PowermeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.powerme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowermeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
