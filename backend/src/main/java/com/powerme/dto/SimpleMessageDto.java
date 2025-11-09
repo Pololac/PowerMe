3f3b9cbf1441757e3212db32833357f892c8ad3f
Pololac <lacostepaul@yahoo.fr>
2025-11-09 20:00:06 +0100

    feat(auth) : jwt, refresh, authService, authController

---
+++ backend/src/main/java/com/powerme/dto/SimpleMessageDto.java
@@ -0,0 +1,19 @@
+package com.powerme.dto;
+
+public class SimpleMessageDto {
+
+    private String message;
+
+    public SimpleMessageDto(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
+
