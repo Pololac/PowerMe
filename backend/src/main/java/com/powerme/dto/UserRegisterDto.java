fc18e2d13c70dcab80c4839a3b380d04f012c009
Pololac <lacostepaul@yahoo.fr>
2025-11-11 14:38:58 +0100

    feat(auth): add messages to UserRegisterDto

--- backend/src/main/java/com/powerme/dto/UserRegisterDto.java
+++ backend/src/main/java/com/powerme/dto/UserRegisterDto.java
@@ -6,11 +6,15 @@ import jakarta.validation.constraints.Size;
 
 public class UserRegisterDto {
 
-    @NotBlank
-    @Email
+    @NotBlank(message = "Email is required")
+    @Email(message = "Please provide a valid email address")
     private String email;
-    @NotBlank
-    @Size(min = 8)
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    // PROD : au moins 1 min, 1 maj, 1 chiffre, 1 caractère spécial et > 8 caractères
+    // @Pattern(regexp = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$",
+    //          message = "Password must include at least one uppercase letter, one lowercase letter, one number and one special character")
     private String password;
 
     public UserRegisterDto() {
