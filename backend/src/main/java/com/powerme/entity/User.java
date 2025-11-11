f02b58d79a0077904ab2fd9c33f282c8e5002c04
Pololac <lacostepaul@yahoo.fr>
2025-11-11 22:43:07 +0100

    fix(auth): change LAZY by EAGER on roles to get them when creating the token

--- backend/src/main/java/com/powerme/entity/User.java
+++ backend/src/main/java/com/powerme/entity/User.java
@@ -58,7 +58,7 @@ public class User implements UserDetails {
      * une Enum. JPA/Hibernate doit stocker cette relation "ManyToMany" dans une table de liaison.
      * {@code @ElementCollection} permet de créer automatiquement cette table de liaison.
      */
-    @ElementCollection(fetch = FetchType.LAZY)
+    @ElementCollection(fetch = FetchType.EAGER)
     @CollectionTable(
             name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id")
