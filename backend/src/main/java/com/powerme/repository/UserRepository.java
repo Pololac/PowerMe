155c057ff2b5c7378744bc35a4f3f098997badb7
Pololac <lacostepaul@yahoo.fr>
2025-11-17 22:58:24 +0100

    fix(JpaRepo): fix type of @Id on RefreshTokenRepo & UserRepo

--- backend/src/main/java/com/powerme/repository/UserRepository.java
+++ backend/src/main/java/com/powerme/repository/UserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     /**
      * Permet de contrôler si un User avec cet emil existe déjà en base.
