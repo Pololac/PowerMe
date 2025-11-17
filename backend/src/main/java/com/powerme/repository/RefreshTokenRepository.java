885b0cf358b3ef3212ee32bcb53d8d2b52ad0f89
Pololac <lacostepaul@yahoo.fr>
2025-11-17 22:58:24 +0100

    fix(JpaRepo): fix type of @Id on RefreshTokenRepo & UserRepo

--- backend/src/main/java/com/powerme/repository/RefreshTokenRepository.java
+++ backend/src/main/java/com/powerme/repository/RefreshTokenRepository.java
@@ -9,7 +9,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
 
     Optional<RefreshToken> findByToken(String token);
 
