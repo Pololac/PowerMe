b8cc7821e32c1d43a27b3110440588d013864156
Pololac <lacostepaul@yahoo.fr>
2025-11-22 17:44:29 +0100

    test(account-service): finalized

--- backend/src/test/java/com/powerme/unit/AccountServiceTest.java
+++ backend/src/test/java/com/powerme/unit/AccountServiceTest.java
@@ -1,9 +1,24 @@
 package com.powerme.unit;
 
+import static com.powerme.enums.Role.ROLE_USER;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.assertArg;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.powerme.entity.User;
+import com.powerme.exception.UserAlreadyExistsException;
 import com.powerme.repository.UserRepository;
 import com.powerme.service.account.AccountServiceImpl;
 import com.powerme.service.mail.MailService;
 import com.powerme.service.security.JwtService;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -15,7 +30,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 class AccountServiceTest {
 
     @Mock
-    private UserRepository userRepository;
+    private UserRepository userRepo;
 
     @Mock
     private PasswordEncoder passwordEncoder;
@@ -27,30 +42,94 @@ class AccountServiceTest {
     private MailService mailService;
 
     @InjectMocks
-    private AccountServiceImpl accountServiceImpl;
+    private AccountServiceImpl accountService;
 
     @Test
-    void registerShouldPersistIfUserNotExists() {
+    void registerShouldSaveUser() {
+        // GIVEN
+        User user = new User("john@test.com", "12345678");
+        when(userRepo.findByEmail(user.getEmail())).thenReturn(Optional.empty());
+        when(passwordEncoder.encode(user.getPassword())).thenReturn("hashed");
 
+        // WHEN
+        accountService.register(user);
+
+        // THEN
+        verify(userRepo, times(1)).save(assertArg(savedUser -> {
+            assertEquals("john@test.com", savedUser.getUsername());
+            assertEquals("hashed", savedUser.getPassword());
+            assertTrue(savedUser.hasRole(ROLE_USER));
+        }));
     }
 
     @Test
-    void registerShouldThrowExceptionWhenEmailAlreadyExists() {
+    void registerShouldHashPassword() {
+        User user = new User("john@test.com", "12345678");
+        when(userRepo.findByEmail(user.getEmail())).thenReturn(Optional.empty());
+        when(passwordEncoder.encode(user.getPassword())).thenReturn("hashed");
 
+        accountService.register(user);
+
+        assertEquals("hashed", user.getPassword());
     }
 
     @Test
-    void registerShouldHashPasswordBeforeSaving() {
+    void registerShouldGenerateTokenAndSendMail() {
+        // GIVEN
+        User user = new User("john@test.com", "12345678");
+        when(userRepo.findByEmail(user.getEmail())).thenReturn(Optional.empty());
+        when(passwordEncoder.encode(user.getPassword())).thenReturn("hashed");
+        when(jwtService.generateToken(eq(user), any())).thenReturn("TOKEN123");
 
+        // WHEN
+        accountService.register(user);
+
+        //THEN
+        // Vérifie que le token a bien été généré
+        verify(jwtService).generateToken(eq(user), any());
+
+        // Vérifie que l’email a été envoyé avec le token exact
+        verify(mailService).sendActivationEmail(user, "TOKEN123");
     }
 
     @Test
-    void registerShouldSendActivationEmail() {
+    void registerShouldThrowExceptionWhenEmailExists() {
+        // GIVEN
+        User user = new User("john@test.com", "12345678");
+        User existing = new User("john@test.com", "abcd");
+        when(userRepo.findByEmail("john@test.com")).thenReturn(Optional.of(existing));
 
+        // WHEN + THEN
+        // Vérifie qu'une exception est levée
+        assertThrows(
+                UserAlreadyExistsException.class,
+                () -> accountService.register(user)
+        );
+
+        //THEN
+        // Vérifie que rien ne s'exécute
+        verify(userRepo, never()).save(any());
+        verify(jwtService, never()).generateToken(any(), any());
+        verify(mailService, never()).sendActivationEmail(any(), any());
     }
+
 
     @Test
     void registerShouldNotSendEmailIfSaveFails() {
+        // GIVEN
+        User user = new User("john@test.com", "12345678");
+        when(userRepo.findByEmail(user.getEmail())).thenReturn(Optional.empty());
+        when(passwordEncoder.encode(user.getPassword())).thenReturn("hashed");
+        // Simule une erreur lors de la sauvegarde en base
+        when(userRepo.save(any()))
+                .thenThrow(new RuntimeException("Database failure"));
 
+        // WHEN + THEN
+        assertThrows(RuntimeException.class, () -> accountService.register(user));
+
+        //THEN
+        // Vérifie qu'aucun token généré ni de mail envoyé
+        verify(jwtService, never()).generateToken(any(), any());
+        verify(mailService, never()).sendActivationEmail(any(), any());
     }
 }
