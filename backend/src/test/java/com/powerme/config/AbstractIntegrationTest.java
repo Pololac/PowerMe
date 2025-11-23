87d8fc58713f46d8802b98114f7fbc191166d12b
Pololac <lacostepaul@yahoo.fr>
2025-11-23 12:36:45 +0100

    chore(testcontainers): replace H2 by testcontainers for Postgre & Postgis compatibility
    - update pom.xml
    - create AbstractIntegrationTest class to configure testcontainers (with Docker)
    
    # Conflicts:
    #	backend/pom.xml

---
+++ backend/src/test/java/com/powerme/config/AbstractIntegrationTest.java
@@ -0,0 +1,36 @@
+package com.powerme.config;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+@Testcontainers
+@SpringBootTest
+@ActiveProfiles("test")
+public abstract class AbstractIntegrationTest {
+
+    private static final DockerImageName POSTGIS_IMAGE =
+            DockerImageName.parse("postgis/postgis:17-3.5")
+                    .asCompatibleSubstituteFor("postgres");
+
+    protected static final PostgreSQLContainer<?> postgres =
+            new PostgreSQLContainer<>(POSTGIS_IMAGE)
+                    .withDatabaseName("testdb")
+                    .withUsername("test")
+                    .withPassword("test");
+
+    static {
+        postgres.start();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+}
