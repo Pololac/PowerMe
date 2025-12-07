c0653229d27a06204ee40929bd2f82020fcc044b
Pololac <lacostepaul@yahoo.fr>
2025-12-07 15:34:44 +0100

    feat(landing): create features components & shared landing navbar

---
+++ frontend/src/app/features/booking/booking.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-booking',
+  imports: [],
+  templateUrl: './booking.html',
+  styleUrl: './booking.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class Booking {
+
+}
---
+++ frontend/src/app/features/charging-stations/charging-stations.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-charging-stations',
+  imports: [],
+  templateUrl: './charging-stations.html',
+  styleUrl: './charging-stations.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ChargingStations {
+
+}
