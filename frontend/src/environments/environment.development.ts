2e33a311bfa75d8ee45b58c5f2a200c3a7dca90d
Pololac <lacostepaul@yahoo.fr>
2025-12-21 00:04:53 +0100

    chore(env): change files dev/prod + add template

---
+++ frontend/src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://localhost:8081/api',
+
+  // Features de développement
+  debug: true,
+  enableMocks: false,
+  logLevel: 'debug',
+
+  // Services externes (clés de test)
+  mapboxToken: 'pk.test.123456',
+  stripePublicKey: 'pk_test_123456',
+};
