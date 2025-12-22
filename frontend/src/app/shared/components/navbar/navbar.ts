a2690055e97380a52ba1b74e57ccb000b23c4802
Pololac <lacostepaul@yahoo.fr>
2025-12-22 21:54:12 +0100

    feat(auth-front): modify Navbar

--- frontend/src/app/shared/components/navbar/navbar.ts
+++ frontend/src/app/shared/components/navbar/navbar.ts
@@ -1,11 +1,11 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../../core/services/auth-service';
 import { UserMenu } from '../user-menu/user-menu';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, UserMenu],
+  imports: [RouterLink, RouterLinkActive, UserMenu],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
