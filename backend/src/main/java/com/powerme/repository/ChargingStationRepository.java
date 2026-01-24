f56a7bf075a829f996b04ccdbba9723c06d763a8
Pololac <lacostepaul@yahoo.fr>
2026-01-24 22:48:56 +0100

    feat(map-back): add method on ChargingStationRepo

--- backend/src/main/java/com/powerme/repository/ChargingStationRepository.java
+++ backend/src/main/java/com/powerme/repository/ChargingStationRepository.java
@@ -1,12 +1,22 @@
 package com.powerme.repository;
 
 import com.powerme.entity.ChargingStation;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ChargingStationRepository extends JpaRepository<ChargingStation, Long> {
+
+    @Query("""
+        select s
+        from ChargingStation s
+        join fetch s.chargingLocation cl
+            join fetch cl.address
+        where s.id = :id
+    """)
+    Optional<ChargingStation> findByIdWithLocationAndAddress(Long id);
 
     @Query("""
         select count(cs)
