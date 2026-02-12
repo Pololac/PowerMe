2af5a61b828dcbd0dae33a43999bdb245f3530c8
Pololac <lacostepaul@yahoo.fr>
2026-02-12 19:37:03 +0100

    fix(front-geocoding): CORS pb on MapTiler request  (authInterceptor send a header "Authorization" ->not allowed by MapTiler API) Solution : add a condition to exclude external requests

--- frontend/src/app/core/interceptors/auth-interceptor.ts
+++ frontend/src/app/core/interceptors/auth-interceptor.ts
@@ -2,18 +2,24 @@ import { HttpInterceptorFn, HttpRequest } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { catchError, concatMap, throwError } from 'rxjs';
 import { AuthService } from '../services/auth-service';
+import { environment } from '../../../environments/environment';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
 
-  const isLogin = req.url.includes('/auth/login');
-  const isRefresh = req.url.includes('/auth/refresh');
-  const isRegister = req.url.includes('/account/register');
-
   const token = localStorage.getItem('token') ?? sessionStorage.getItem('token');
 
-  // Ne jamais intercepter les routes d'auth publiques
-  if (isLogin || isRegister || isRefresh) {
+  // Ne jamais intercepter si :
+  //  - requête externe (MapTiler, Stripe, etc.)
+  //  - routes publiques auth
+  //  - pas de token
+  const isBackendRequest = req.url.startsWith(environment.apiBaseUrl);
+  const isPublicAuthRoute =
+    req.url.includes('/auth/login') ||
+    req.url.includes('/auth/refresh') ||
+    req.url.includes('/account/register');
+
+  if (!token || !isBackendRequest || isPublicAuthRoute) {
     return next(req);
   }
 
